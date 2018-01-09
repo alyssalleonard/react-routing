a346a51c2fb1152aac539128ddfddb230153a7d1
Alyssa Leonard <leonard.alyssa@me.com>
2018-01-08 23:39:28 -0700

    Completed mini-project

---
+++ src/Route2.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Route2() {
+    return(
+        <div> Route Two </div>
+    )
+}
---
+++ src/Route3.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Route3() {
+    return(
+        <div> Route Three </div>
+    )
+}
